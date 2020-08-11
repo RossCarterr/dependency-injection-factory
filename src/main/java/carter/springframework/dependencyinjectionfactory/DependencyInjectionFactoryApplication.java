7978afa980c82bc4a7a0841ff3688aa3be7a87e4
RossCarterr <ross29.8@hotmail.com>
2020-08-11 15:38:25 +0100

    FakeDataSource setup

--- src/main/java/carter/springframework/dependencyinjectionfactory/DependencyInjectionFactoryApplication.java
+++ src/main/java/carter/springframework/dependencyinjectionfactory/DependencyInjectionFactoryApplication.java
@@ -4,6 +4,7 @@ import carter.springframework.dependencyinjectionfactory.controllers.Constructor
 import carter.springframework.dependencyinjectionfactory.controllers.GetterInjectedController;
 import carter.springframework.dependencyinjectionfactory.controllers.MyController;
 import carter.springframework.dependencyinjectionfactory.controllers.PropertyInjectedController;
+import carter.springframework.dependencyinjectionfactory.exampleBeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +17,13 @@ public class DependencyInjectionFactoryApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
+
+//		Bean testing
+//		System.out.println(controller.hello());
+//		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+//		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+//		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
